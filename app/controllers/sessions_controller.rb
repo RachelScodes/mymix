faf6fb8e4b3eb9da5bb4647efd4dbc6b0811e6b2
Anita Growsoon <anitagrowsoon@gmail.com>
2015-11-05 22:18:09 -0500

    can add users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
-class Sessions < ApplicationController
+class SessionsController < ApplicationController
 
    def new
    end
 
    def create
-      user = User.find_by_email(params[:email])
+      current_user = User.find_by(dj_name: params[:dj_name])
 
       # If the user exists AND the password entered is correct.
-      if user && user.authenticate(params[:password])
+      if current_user && current_user.authenticate(params[:password])
          # Save the user id inside the browser cookie. This is how we keep the user
          # logged in when they navigate around our website.
-         session[:current_user_id] = @user.id
+         session[:current_user_id] = current_user.id
          redirect_to '/'
       else
          # If user's login doesn't work, send them back to the login form.
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,28 @@ class UsersController < ApplicationController
    end
 
    def create
-     user = User.new(user_params)
-     if user.save
-       session[:user_id] = user.id
-       redirect_to '/'
+      @user = User.new(user_params)
+     if @user.save
+       session[:user_id] = @user.id
+       redirect_to "/users/#{@user.id}"
      else
        redirect_to '/signup'
      end
    end
 
+   def index
+      @users = User.all
+   end
+
+   def show
+      @user = User.find(params[:id])
+   end
+
    private
 
    def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:dj_name, :email, :avatar, :password, :password_confirmation)
    end
+
 
 end
