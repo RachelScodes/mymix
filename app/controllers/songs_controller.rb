b8a84de9ef80b0097b6589db45331af7f0d71fdc
Anita Growsoon <anitagrowsoon@gmail.com>
2015-11-06 08:02:20 -0500

    updated readme

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,16 +4,21 @@ class SongsController < ApplicationController
 
    def create
      @song = Song.new(song_params)
-     @song.mixtape_id = params[:mixtape_id]
-
      @song.save
+     @rec = @song.recordings.create
+     hit_record
 
-     redirect_to mixtape_path(@song.mixtape)
+     redirect_to mixtape_path
    end
+
+   def hit_record
+      @rec.mixtape_id = params[:id]
+   end
+
+   private
 
    def song_params
-     params.require(:song).permit(:author_name, :body)
+     params.require(:song).permit(:title, :artist, :album, :date_released, :src_url, :user_id)
    end
-
 
 end
